dbe2c13434eb8e4f2bdc62cc766d9b47db4dbc7d
Kevin C Lee <cykevinlee at gmail dot com>
2016-06-01 10:20:48 +1200

    Added file cleanup() to individual folders and qiime() for optional downstream analysis.

--- superscript.sh
+++ superscript.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 # Prints welcome message and takes project name
-function welcome {
-echo "PE Illumina FASTQ to biom superscript alpha v0.4"
-echo "Developed in Z-shell (zsh)"
+welcome() {
+echo "PE Illumina FASTQ to biom superscript alpha v1.0"
+echo "Developed for Z-shell (zsh)"
 echo "--------------------------------------------------------"
 echo "Dependencies:"
 echo "The following commands need to be in the \$PATH variable, and named as such:"
@@ -10,6 +10,7 @@ echo "mothur - from www.mothur.org"
 echo "uc2otutab.py - from www.drive5.com"
 echo "biom - from biom-format.org"
 echo "usearch8 - from www.drive5.com"
+echo "Optional qiime integration (in Python 2)."
 echo "--------------------------------------------------------" 
 echo "This script requires Python 2 due to the use of uc2otutab.py from drive5"
 echo "Place all and only the paired-end Illumina files of the this project in this local folder."
@@ -18,9 +19,8 @@ echo "Enter the project name that will be given to the output files as an identi
 read project
 echo "Project name: $project"
 }
-
 # Picks representative OTU sequences using usearch8 and mothur, they need to be in $PATH
-function otu {
+otu() {
 echo "Merging reads..."
 for file in *R1_001.fastq
 do
@@ -47,7 +47,6 @@ cat $filename >> "${project}_sequence.fasta"
 done
 
 # Hardcoded mothur trimming parameters
-
 echo "Sequence trimming, dereplication, singleton removal, and clustering"
 mothur "#trim.seqs(fasta="${project}_sequence.fasta", minlength=200, maxlength=500, maxhomop=6)"
 usearch8 -derep_fulllength "${project}_sequence.trim.fasta" -fastaout "${project}_seq_derep.fasta" -sizeout
@@ -59,13 +58,14 @@ fasta_number.py "${project}_rep_set.fasta" "OTU_" > "${project}_rep_numbered.fas
 }
 
 #Abundance calculation by using unfiltered but trimmed reads
-function abundance {
+abundance() {
 echo "Generating unfiltered reads for abundance information..."
 
 for file in *merged.fastq; do
     sampleID=$(echo "$file" | cut -d "_" -f1)
     usearch8 -fastq_filter "${sampleID}_merged.fastq" -fastaout "${sampleID}_unfiltered.fasta" #-fastq_maxee 1.0 <- note the exclusion
 done &> unfiltered_log.txt
+
 
 for filename in $(ls *_unfiltered.fasta); do 
 samplename=$(echo $filename | cut -d '_' -f1)
@@ -80,17 +80,72 @@ echo "Mapping the abundance data..."
 usearch8 -usearch_global "${project}_abundance.trim.fasta" -db "${project}_rep_numbered.fasta" -id 0.97 -strand plus -uc "${project}_readmap.uc"
 }
 
+cleanup() {
+echo "Cleaning up..."
+mkdir log
+mv unfiltered_log.txt log/
+mv merge_log.txt log/
+mv filter_log.txt log/
+
+mkdir sequences
+mv *.fasta sequences/
+mv *.fastq sequences/
+cp "sequences/${project}_rep_numbered.fasta" ./
+}
+
+# Optional QIIME analysis, these won't work with non-16S data (e.g., ITS, 18S, rbcL)
+qiime() {
+echo "Activating python2 envrionment for QIIME..."
+source activate python2
+
+echo "Assigning taxonomy, this might take a while."
+assign_taxonomy.py -i "${project}_rep_numbered.fasta" -m rdp -o rdp_tax
+
+echo "Adding taxonomy to biom file, use this biom file for downstream analysis from here on."
+biom add-metadata --sc-separated taxonomy --observation-header OTUID,taxonomy --observation-metadata-fp "rdp_tax/${project}_rep_numbered_tax_assignments.txt"  -i "${project}.biom" -o "${project}_tax.biom"
+
+echo "Making taxonomy stack bar graphs. Using the catchall 'summarize_taxa_through_plots.py' script"
+summarize_taxa_through_plots.py -i "${project}_tax.biom" -o "${project}_tax_summary"
+
+echo "Conducting three steps to produce a phylogenetic tree, for phylogeny-based analysis downstream (Unifrac, PD_whole_tree)."
+echo "Aligning representative OTU sequence."
+align_seqs.py -i "${project}_rep_numbered.fasta"
+echo "Filtering the alignment."
+filter_alignment.py -i "pynast_aligned/${project}_rep_numbered_aligned.fasta"
+echo "Making the phylogenetic tree."
+make_phylogeny.py -i "${project}_rep_numbered_aligned_pfiltered.fasta"
+
+echo "All done! Now exit python2 environment."
+source deactivate
+}
+
 welcome
 otu
 abundance
+cleanup
 
+# Make biom file from classic OTU table
 # This is dependent on my anaconda python2 environment set up, rename to your env name
 echo "Activating python2 environment for .uc --> txt script" 
 source activate python2
 
-# convert uc output to "classic" OTU table
+
+# I would love to get this section into a funciton, but this script below
+# won't work within the scope of a shell function for some reason (will get stuck in a loop)
+echo "Converting uc output to classic OTU table"
 uc2otutab.py "${project}_readmap.uc" > "${project}_readmap.txt"
 
 # convert "classic" otu table to biom file
 echo "Output HDF5 biom OTU table"
 biom convert -i "${project}_readmap.txt" -o "${project}.biom" --table-type="OTU table" --to-hdf5
+
+echo "Deactivate python2 environment, may be activated again for QIIME downstream."
+source deactivate
+
+read -q "response?Do you want to conduct downstream QIIME analysis? [y/N] "
+if [[ "$response" =~ ^[Yy]$ ]]
+then
+qiime
+fi
+
+echo "Analysis completed. Hope you enjoyed your coffee!"
